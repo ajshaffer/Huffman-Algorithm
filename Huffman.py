e413ebe746639779ffeab5d0d6d8a66db07a77f7
Austin Shaffer <ajshaffer@coastal.edu>
2023-12-04 13:21:43 -0500

    Setting up project

--- Huffman.py
+++ Huffman.py
@@ -6,6 +6,13 @@
     Once the tree has been built, your program will be able to:
     1. encode a sequence of characters into a string of 1’s and 0’s, and
     2. decode a string of 1’s and 0’s into a sequence of characters.
+
+    -Initialize n 1-node trees with alphabet characters and the tree weights with their frequencies
+        ex. F - .25
+    -Repeat the following step n-1 times:
+        -join 2 binary trees with smallest weights into 1 (left and right subtrees)
+        -make its weight equal the sum of the weights of the 2 trees
+    -Mark edges leading to the left and right subtrees with 0's and 1's, respectively 
 """
 
 
