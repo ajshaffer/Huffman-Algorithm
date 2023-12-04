9454cab86dbd5a5dc58edd02ec484a6b39a89e80
Austin Shaffer <ajshaffer@coastal.edu>
2023-12-04 12:39:28 -0500

    Setting up project

--- Huffman.py
+++ Huffman.py
@@ -1,5 +1,13 @@
 
 
+"""
+    Implement Huffman’s algorithm in the language of your choice using a min-priority queue. 
+    Given a table of characters and their frequency, construct a Huffman tree. 
+    Once the tree has been built, your program will be able to:
+    1. encode a sequence of characters into a string of 1’s and 0’s, and
+    2. decode a string of 1’s and 0’s into a sequence of characters.
+"""
+
 
 with open('/Users/austinshaffer/Desktop/dataTeamX.txt', 'r') as f:
     contents = f.readlines()
@@ -8,4 +16,4 @@ f.close()
 for line in contents:
     print(line)
 
-    
+
