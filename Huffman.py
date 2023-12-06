b94ddda5d192787aed6a22b755736b0bfe0552ee
Austin Shaffer <ajshaffer@coastal.edu>
2023-12-06 16:32:35 -0500

    working Huffman tree with encoding and decoding

--- Huffman.py
+++ Huffman.py
@@ -1,25 +1,8 @@
 
 import heapq
 
-
-
-"""
-    Implement Huffman’s algorithm in the language of your choice using a min-priority queue. 
-    Given a table of characters and their frequency, construct a Huffman tree. 
-    Once the tree has been built, your program will be able to:
-    1. encode a sequence of characters into a string of 1’s and 0’s, and
-    2. decode a string of 1’s and 0’s into a sequence of characters.
-
-    -Initialize n 1-node trees with alphabet characters and the tree weights with their frequencies
-        ex. F - .25
-    -Repeat the following step n-1 times:
-        -join 2 binary trees with smallest weights into 1 (left and right subtrees)
-        -make its weight equal the sum of the weights of the 2 trees
-    -Mark edges leading to the left and right subtrees with 0's and 1's, respectively 
-"""
-
-encode_path = "/Users/austinshaffer/Desktop/dataTeamX.txt"
-decode_path = ""
+# encode_path = "/Users/austinshaffer/Desktop/dataTeamX.txt"
+# decode_path = ""
 
 
 class HuffmanAlgorithm:
@@ -46,21 +29,23 @@ class HuffmanAlgorithm:
         return char_weight
 
 
+
+user_choice = int(input("Enter 1 to Encode or 2 to Decode:"))
+
+if user_choice == 1:
+    encode_path = input("Enter the path of the .txt file to encode:")
     
-    
-            
+elif user_choice == 2:
+    decode_path = input("Enter the path of the .txt file to decode:")
 
-    
-
-
-
-
-
+else: 
+    print("No valid input detected.")
 
 huffman_instance = HuffmanAlgorithm(None, None)
 freq_table = huffman_instance.create_freq_table()
 print("Frequency table:", freq_table)
    
+
 
 
 
