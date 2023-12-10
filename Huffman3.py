dfa0afa8a5bcfe6d49b189d8cd3121d754d1f0a4
Austin Shaffer <ajshaffer@coastal.edu>
2023-12-10 17:14:38 -0500

    Sharing changes

--- Huffman3.py
+++ Huffman3.py
@@ -33,6 +33,15 @@ def build_huffman_tree(data):
 
     return nodes[0]
 
+def read_data_from_file(filename):
+    # Open the file and read character frequencies
+    with open(filename, "r") as f:
+        data = []
+        for line in f:
+            char, weight = line.split()
+            data.append((char, float(weight)))
+    return data
+
 
 def huffman_encoding(data):
     tree = build_huffman_tree(data)
@@ -72,14 +81,19 @@ def huffman_decoding(encoded_data, tree):
     return decoded_data
 
 
-def read_data_from_file(filename):
-    # Open the file and read character frequencies
-    with open(filename, "r") as f:
-        data = []
-        for line in f:
-            char, weight = line.split()
-            data.append((char, float(weight)))
-    return data
+def encode_or_decode():
+    choice = input("Enter 'encode' or 'decode': ").lower()
+
+    if choice == "encode":
+        data = input("Enter the string to encode: ")
+        encoded_data = huffman_encoding(data)
+        print("Encoded data:", encoded_data)
+    elif choice == "decode":
+        encoded_data = input("Enter the encoded data: ")
+        decoded_data = huffman_decoding(encoded_data, tree)
+        print("Decoded data:", decoded_data)
+    else:
+        print("Invalid choice. Please enter 'encode' or 'decode'.")
 
 filename = "/Users/austinshaffer/Desktop/dataTeamX.txt"
 data = read_data_from_file(filename)
@@ -87,8 +101,4 @@ data = read_data_from_file(filename)
 chars = [char for char, freq in data]
 tree = build_huffman_tree(chars)
 
-encoded_data = huffman_encoding(data)
-print("Encoded data:", encoded_data)
-
-decoded_data = huffman_decoding(encoded_data, tree)
-print("Decoded data:", decoded_data)
+encode_or_decode()
